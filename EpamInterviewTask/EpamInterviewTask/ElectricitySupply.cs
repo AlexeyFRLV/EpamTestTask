61b9d5d967b992fda4d40e8a0e8bd96fbd2a6cee
Alexey Frolov <foritlearning@gmail.com>
2020-10-24 00:15:39 +0300

    added the "ElectricitySupply" class

---
+++ EpamInterviewTask/EpamInterviewTask/ElectricitySupply.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EpamInterviewTask
+{
+    class ElectricitySupply
+    {
+        Client[] clients;                                                               //список клиентов
+        public decimal SUM { get; set; } = 0;                                           //общая сумма платежей всех клиентов
+        public decimal LG { get; set; } = 0;                                            //общий размер льготы
+
+
+        public ElectricitySupply(Client[] clients)
+        {
+            this.clients = clients;
+        }
+
+    }
+}
