3ab473e7e16bd8c6c35ad8e3f6810aca57398d13
Alexey Frolov <foritlearning@gmail.com>
2020-10-24 00:20:01 +0300

    added methods to sort by the amount of payments and type of clients

--- EpamInterviewTask/EpamInterviewTask/ElectricitySupply.cs
+++ EpamInterviewTask/EpamInterviewTask/ElectricitySupply.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections;
 
 namespace EpamInterviewTask
 {
-    class ElectricitySupply
+    class ElectricitySupply : IEnumerable
     {
         Client[] clients;                                                               //список клиентов
         public decimal SUM { get; set; } = 0;                                           //общая сумма платежей всех клиентов
@@ -14,5 +15,54 @@ namespace EpamInterviewTask
             this.clients = clients;
         }
 
+        //Метод сортировки клиентов по количесвту потребленной эл. энергии
+        public void Sort()
+        {
+            Array.Sort(clients);
+        }
+
+        //Метод сортировки клиентов по величине платежа
+        public void Sort(IComparer comparer)
+        {
+            Array.Sort(clients, comparer);
+        }
+
+        //Метод сортировки клиентов по типу клиентов
+        public void StatusSort(IComparer comparer)
+        {
+            Array.Sort(clients, comparer);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return clients.GetEnumerator();
+        }
     }
+
+    //Вспомогательный класс интерфейса IComparer для перегрузки сортировки по "Платежам"
+    class PaymentComparer : IComparer
+    {
+        public int Compare(object ob1, object ob2)
+        {
+            if (ob1 is Client && ob2 is Client)
+            {
+                return (int)Math.Round(((ob1 as Client).Payment - (ob2 as Client).Payment), MidpointRounding.AwayFromZero);
+            }
+            throw new NotImplementedException();
+        }
+    }
+
+    //Вспомогательный класс интерфейса IComparer для упорядочивания по типу клиентов
+    class StatusComparer : IComparer
+    {
+        public int Compare(object ob1, object ob2)
+        {
+            if (ob1 is Client && ob2 is Client)
+            {
+                return (ob1 as Client).Status - (ob2 as Client).Status;
+            }
+            throw new NotImplementedException();
+        }
+    }
+
 }
