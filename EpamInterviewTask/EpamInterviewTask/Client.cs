25d29803d6ec3465f841fe56668274c6a6011bc3
Alexey Frolov <foritlearning@gmail.com>
2020-10-24 00:12:14 +0300

    added method for calculating the payment amount

--- EpamInterviewTask/EpamInterviewTask/Client.cs
+++ EpamInterviewTask/EpamInterviewTask/Client.cs
@@ -23,6 +23,31 @@ namespace EpamInterviewTask
             throw new NotImplementedException();
         }
 
+        //Вычисление суммы платежа за затраченную эл. энергию
+        public void PaymentCalculation()
+        {
+            switch (Status)
+            {
+                case TypeOfClient.normal:
+                    Payment = Math.Round(ElEnergy * Tariff, 2);
+                    break;
+                case TypeOfClient.limited:
+                    if (ElEnergy <= 150)
+                        Payment = Math.Round(ElEnergy * Tariff, 2);
+                    if (ElEnergy > 150)
+                        Payment = Math.Round(150 * Tariff + ((ElEnergy - 150) * (Tariff + Tariff * 0.33m)), 2);
+                    break;
+                case TypeOfClient.withBenifits1:
+                    Payment = Math.Round(ElEnergy * Tariff * 0.66m, 2);
+                    break;
+                case TypeOfClient.withBenifits2:
+                    if (ElEnergy <= 50)
+                        Payment = 0.0m;
+                    if (ElEnergy > 50)
+                        Payment = Math.Round((ElEnergy - 50) * Tariff, 2);
+                    break;
+            }
+        }
 
         //Конвертирование перечислимого типа в строку для вывода типа клиентов
         public string ConvertStatus()
