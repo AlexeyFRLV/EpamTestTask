97651b4b4fee17283ba3bff749714ce8044e1f7d
Alexey Frolov <foritlearning@gmail.com>
2020-10-23 23:52:24 +0300

    added the "Human" class

---
+++ EpamInterviewTask/EpamInterviewTask/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EpamInterviewTask
+{
+    class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"Имя: {FirstName}\tФамилия: {LastName}\tДата рождения: {BirthDate.ToShortDateString()}";
+        }
+    }
+}
